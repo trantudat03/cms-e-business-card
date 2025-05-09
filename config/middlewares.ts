3e8310970d8b594569a163f9fb15c4d1a3d3a47b
Dat Tran <dat.tran@kyanon.digital>
2025-05-09 18:01:41 +0700

    feature upload image on cloudinary

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,15 @@ export default [
   {
     name: "strapi::security",
     config: {
-      contentSecurityPolicy: false, // Tắt CSP để tránh lỗi CORS
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
     },
   },
   {
